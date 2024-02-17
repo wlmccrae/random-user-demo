61df3efa62c049af3d1d3643f497b30d53859e89
Wanda McCrae <wanda.mccrae@gmail.com>
2024-02-17 18:49:49 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from fastapi import FastAPI
 
 from routers import users
 
-app = FastAPI()
+description = """
+A FastAPI backend that fetches randomly generated users from the Random User Generator API and saves them to a PostgreSQL database.
+"""
+
+app = FastAPI(
+    title="Random User Generator Demo",
+    description=description,
+    version="1.0",
+    contact={
+        "name": "Wanda McCrae",
+        "url": "https://wlmccrae.wordpress.com"
+    },
+)
 app.include_router(users.router, tags=["USERS"])
