182f3d370b77b569a923a96001449e88fb9148b3
Wanda McCrae <wanda.mccrae@gmail.com>
2024-03-09 23:58:11 -0500

    completed unit testing

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -6,6 +6,20 @@ client = TestClient(app)
 
 
 class FakeUserQueries:
+    def create_user(self, user_info):
+        # The route expects data to be passed into the real query,
+        # so we need to include that parameter in the fake query
+        # even though it isn't actually defined or used.
+        return {
+            "id": 13,
+            "name": "Miro Peltola",
+            "dob": "1947-11-22T00:52:50.790Z",
+            "email": "miro.peltola@example.com",
+            "city": "Tervo",
+            "state": "Southern Savonia",
+            "country": "Finland"
+        }
+
     def get_user(self, user_id):
         return {
             "id": user_id,
@@ -48,6 +62,42 @@ class FakeUserQueries:
             }
         ]
 
+    def update_user(self, user_id):
+        return {
+            "id": user_id,
+            "name": "Miro Peltola",
+            "dob": "1947-11-22T00:52:50.790Z",
+            "email": "miro.peltola@example.com",
+            "city": "Tervo",
+            "state": "Southern Savonia",
+            "country": "Finland"
+        }
+
+    def delete_user(self, user_id):
+        return True
+
+
+def test_create_user():
+    # Arrange
+    app.dependency_overrides[UserQueries] = FakeUserQueries
+
+    # Act
+    res = client.post('/app/users')
+    data = res.json()
+
+    # Assert
+    assert res.status_code == 200
+    assert "id" in data
+    assert "name" in data
+    assert "dob" in data
+    assert "email" in data
+    assert "city" in data
+    assert "state" in data
+    assert "country" in data
+
+    # Cleanup
+    app.dependency_overrides = {}
+
 
 def test_get_user():
     # Arrange
@@ -60,6 +110,12 @@ def test_get_user():
     # Assert
     assert res.status_code == 200
     assert data["id"] == 55
+    assert "name" in data
+    assert "dob" in data
+    assert "email" in data
+    assert "city" in data
+    assert "state" in data
+    assert "country" in data
 
     # Cleanup
     app.dependency_overrides = {}
@@ -75,7 +131,50 @@ def test_get_users():
 
     # Assert
     assert res.status_code == 200
+    assert 'users' in data
     assert len(data["users"]) == 3
+
+    # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_update_user():
+    # Arrange
+    app.dependency_overrides[UserQueries] = FakeUserQueries
+    input_data = {
+        "name": "Wanda Lotus"
+    }
+
+    # Act
+    res = client.get('/app/users/42')
+    data = res.json()
+
+    # Assert
+    assert res.status_code == 200
+    assert data["id"] == 42
+    assert "name" in data
+    assert "dob" in data
+    assert "email" in data
+    assert "city" in data
+    assert "state" in data
+    assert "country" in data
+    assert "name" != input_data["name"]
+
+    # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_delete_user():
+    # Arrange
+    app.dependency_overrides[UserQueries] = FakeUserQueries
+
+    # Act
+    res = client.delete('/app/users/12')
+    data = res.json()
+
+    # Assert
+    assert res.status_code == 200
+    assert data is True
 
     # Cleanup
     app.dependency_overrides = {}
